6348d9c4d97b6535cb89575bb557d3d161ce5cbd
mysql73 <arfan2173@gmail.com>
2022-01-29 01:53:54 +0700

    add body class

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,15 @@
 <template>
-  <router-view class="container-md"></router-view>
+  <router-view class="container-md custom-container pt-5 mt-5"></router-view>
 </template>
+
+<script setup>
+import { onMounted } from 'vue';
+
+onMounted(() => {
+  document.body.classList.add("bg-light");
+});
+
+</script>
 
 <style>
 a {
