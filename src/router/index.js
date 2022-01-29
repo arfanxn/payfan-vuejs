981cff8ab274ee940dd7a48c3c348a2007b065c4
mysql73 <arfan2173@gmail.com>
2022-01-30 03:35:47 +0700

    change title

--- src/router/index.js
+++ src/router/index.js
@@ -38,7 +38,7 @@ const routes = [{
   // },
   {
     path: '/transfer',
-    name: 'Send And Req',
+    name: 'Send And Request',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
