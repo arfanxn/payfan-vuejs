97c5e9b13a8d33094eaab76f85b7e536e5bf70eb
mysql73 <arfan2173@gmail.com>
2022-02-01 03:13:12 +0700

    edit 404pagentfound

--- src/views/Errors/404PageNotFound.vue
+++ src/views/Errors/404PageNotFound.vue
@@ -1,17 +1,24 @@
 <template>
     <main class="d-flex justify-content-center">
-        <div class="my-auto text-center bg-white p-5 rounded-pill">
-            <router-link
-                to="/"
-                class="fs-1 text-navy rounded my-auto fw-bold text-decoration-underline"
-            >Payfan</router-link>
-            <h1 class="fs-1 mx-5 text-dark my-auto fw-bold">404 | Page Not Found</h1>
+        <div class="my-auto text-center bg-white p-5 rounded">
+            <div class="mb-3">
+                <a href="/" class="fs-1 text-navy rounded my-auto fw-bold">Payfan</a>
+            </div>
+            <h2 class="mx-5 text-dark my-auto fw-bold mb-2">404 | Page Not Found</h2>
+
+            <a href="/">
+                <small class="text-navy hover-underline text-dark">Go to Dashboard</small>
+            </a>
         </div>
     </main>
 </template>
 
 <script setup>
+import { onMounted } from "@vue/runtime-core";
 
+onMounted(() => {
+    document.body.classList.add("bg-navy");
+})
 </script>
 
 <style scoped>
