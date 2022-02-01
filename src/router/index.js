612cd1c7efc27dd290a3c9d80ddf8df85385a534
mysql73 <arfan2173@gmail.com>
2022-02-01 09:01:33 +0700

    add new test route

--- src/router/index.js
+++ src/router/index.js
@@ -61,6 +61,14 @@ const routes = [{
     // which is lazy-loaded when the route is visited.
     component: () => import( /* webpackChunkName: "about" */ '../views/Help.vue')
   },
+  {
+    path: '/test/view',
+    name: 'TestView',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import( /* webpackChunkName: "about" */ '../views/TestView.vue')
+  },
 ]
 
 
