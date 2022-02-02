92c4ab1f01841921dc30f2b96b45510018cb0c0c
mysql73 <arfan2173@gmail.com>
2022-02-02 14:33:56 +0700

    integrate with icons/staricon

--- src/components/SendAndRequest/Contacts.vue
+++ src/components/SendAndRequest/Contacts.vue
@@ -19,32 +19,21 @@
                 v-for="(item, index) in [1, 2, 3, 4, 5]"
                 :key="index"
             >
-                <router-link to="/" class="d-flex text-dark w-100 me-2">
+                <a @click.prevent="contactDetail(item)" class="d-flex text-dark w-100 me-2">
                     <div
                         class="contact-icon-size shadow bg-warning rounded-circle d-flex justify-content-center"
                     >
                         <span class="fw-bold my-auto">AM</span>
                     </div>
                     <span class="ms-3 my-auto">Alif Maulana</span>
-                </router-link>
+                </a>
 
                 <div
                     @click="addOrRmFavoriteContact(index)"
                     :id="`top-contact-${index}`"
                     class="my-auto text-dark"
                 >
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="24"
-                        height="24"
-                        fill="currentColor"
-                        class="bi bi-star-fill my-auto"
-                        viewBox="0 0 16 16"
-                    >
-                        <path
-                            d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"
-                        />
-                    </svg>
+                    <StarIcon width="24" height="24" />
                 </div>
             </div>
 
@@ -54,12 +43,24 @@
                 >View blocked contacts</small>
             </div>
         </div>
+
+        <ContactDetailModal />
     </div>
 </template>
 
 <script setup>
+import StarIcon from '@/components/Icons/StarIcon.vue';
+import ContactDetailModal from '../Bootstrap5/Modals/ContactDetailModal.vue';
+import { defineComponent } from '@vue/runtime-core';
+import Helpers from '../../Helpers';
+defineComponent({ StarIcon, ContactDetailModal })
 
 
+
+function contactDetail(data) {
+    Helpers.triggerBSModal("#btn-modal-contact-detail");
+    console.log(data);
+}
 function addOrRmFavoriteContact(id) {
     const elem = document.getElementById(`top-contact-${id}`);
     if (elem.classList.contains("text-dark")) {
