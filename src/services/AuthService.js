6b8465b7ea4240797a3abe985fa44e4184da325e
mysql73 <arfan2173@gmail.com>
2022-02-05 19:19:21 +0700

    make auth func

--- src/services/AuthService.js
+++ src/services/AuthService.js
@@ -13,14 +13,39 @@ export default class AuthService {
         }
     }
 
-    static async register(fullname, email, password, password_confirmation) {
+    static async register(fullname, email, password, password_confirmation, code) {
         try {
             const response = await axios.post("/api/user/register", {
                 name: fullname,
                 fullname: fullname, // just in case if the server only accept fullname  
                 email,
                 password,
-                password_confirmation
+                password_confirmation,
+                code
+            });
+            return response;
+        } catch (error) {
+            return error.response;
+        }
+    }
+
+    static async createVerificationCode(email) {
+        try {
+            const response = await axios.post("/api/user/verification-code/create", {
+                email,
+            });
+            return response;
+        } catch (error) {
+            return error.response;
+        }
+    }
+
+    static async verifyVerificationCode(email, otp, hashed_code) {
+        try {
+            const response = await axios.post("/api/user/verification-code/verify", {
+                email,
+                otp,
+                hashed_code
             });
             return response;
         } catch (error) {
