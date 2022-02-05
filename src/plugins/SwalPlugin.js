cde434aa50be059bfcfa303972f31f444e960fd5
mysql73 <arfan2173@gmail.com>
2022-02-05 19:20:18 +0700

    add modal

--- src/plugins/SwalPlugin.js
+++ src/plugins/SwalPlugin.js
@@ -1,8 +1,54 @@
 import Swal from "sweetalert2";
+// import AuthService from "../services/AuthService";
 
 export default class SwalPlugin {
     static close() {
         return Swal.close();
+    }
+
+    static async alert(title, html) {
+        return await Swal.fire({
+            title,
+            html,
+            allowEscapeKey: true,
+            allowOutsideClick: true,
+            closeOnClickOutside: true,
+            showCancelButton: false, // There won't be any cancel button
+            showConfirmButton: false, // There won't be any confirm button 
+            buttons: false,
+            closeOnConfirm: false,
+        });
+    }
+
+    static async showVerificationCode(title = 'Verify') {
+        return await Swal.fire({
+            title,
+            html: `<input type="text" id="verification-code" class="swal2-input " placeholder="Verification Code"><br><small class="text-break">OTP has been sent to your email.</small>`,
+            allowEscapeKey: false,
+            allowOutsideClick: false,
+            closeOnClickOutside: false,
+            closeOnConfirm: false,
+            confirmButtonText: 'Next',
+            confirmButtonColor: "#003087",
+            focusConfirm: false,
+            preConfirm: () => {
+                const verificationCode = Swal.getPopup().querySelector('#verification-code');
+                const verificationCodeValue = verificationCode.value;
+
+                if (verificationCodeValue.length != 6) {
+                    Swal.showValidationMessage("Verification Code must be 6 chars!");
+                }
+                if ((verificationCodeValue).match(/\D/)) {
+                    Swal.showValidationMessage("Please enter number only!");
+                }
+                if (!verificationCodeValue) {
+                    Swal.showValidationMessage(`Please enter Verification Code!`);
+                }
+                return {
+                    verification_code: verificationCodeValue
+                };
+            }
+        });
     }
 
     static async showLoading(callback = null) {
