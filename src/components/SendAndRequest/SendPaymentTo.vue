e6a98d7f562d3a4733c0f0bb757a53b2235b87df
mysql73 <arfan2173@gmail.com>
2022-02-05 19:19:57 +0700

    add component

--- src/components/SendAndRequest/SendPaymentTo.vue
+++ src/components/SendAndRequest/SendPaymentTo.vue
@@ -9,12 +9,17 @@
                 placeholder="Name on your contacts or Email"
             />
             <label class="text-secondary" for="inputSendTo">Name on your contacts or Email</label>
+            <!-- <DropdownInputContactOption /> -->
         </div>
         <button class="btn btn-light border-dark rounded-pill py-2 px-4">Next</button>
     </div>
 </template>
 
-<script setup></script>
+<script setup>
+import DropdownInputContactOption from "@/components/SendAndRequest/DropdownInputContactOption.vue";
+import { defineComponent } from "@vue/runtime-core";
+defineComponent({ DropdownInputContactOption }); 
+</script>
 
 <style scoped>
 </style>
