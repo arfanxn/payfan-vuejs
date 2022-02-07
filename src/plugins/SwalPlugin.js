7ab8b1d7afd09d84322d12de869efa4c290027e3
mysql73 <arfan2173@gmail.com>
2022-02-08 02:33:31 +0700

    edit  verificationCode  and make  autoclosealert

--- src/plugins/SwalPlugin.js
+++ src/plugins/SwalPlugin.js
@@ -1,4 +1,6 @@
 import Swal from "sweetalert2";
+import Helpers from "../Helpers";
+import AuthService from "../services/AuthService";
 // import AuthService from "../services/AuthService";
 
 export default class SwalPlugin {
@@ -6,7 +8,7 @@ export default class SwalPlugin {
         return Swal.close();
     }
 
-    static async alert(title, html) {
+    static async alert(title = Helpers.ENV("APP_TITLE"), html = "") {
         return await Swal.fire({
             title,
             html,
@@ -20,33 +22,73 @@ export default class SwalPlugin {
         });
     }
 
-    static async showVerificationCode(title = 'Verify') {
+    static async verificationCode(title = 'Verify', onConfirmCallback, resendCallbackOrObjectOrEmail = null) {
         return await Swal.fire({
             title,
-            html: `<input type="text" id="verification-code" class="swal2-input " placeholder="Verification Code"><br><small class="text-break">OTP has been sent to your email.</small>`,
+            html: `<input type="text" id="verification-code" class="swal2-input " placeholder="Verification Code"><br><small class="text-break">OTP has been sent to your email.</small>
+            <div class=" pt-2 pb-1 text-center ">
+                <button id="resend" class="btn btn-light">Resend<span class="ms-1"></span></button>
+                <span class="px-1"></span>
+                <button id="next"
+                class="btn btn-primary"
+                style="background-color:#003087 !important;">Next</button>
+            </div>`,
             allowEscapeKey: false,
             allowOutsideClick: false,
+            showCancelButton: false,
+            showConfirmButton: false,
             closeOnClickOutside: false,
             closeOnConfirm: false,
             confirmButtonText: 'Next',
             confirmButtonColor: "#003087",
             focusConfirm: false,
-            preConfirm: () => {
-                const verificationCode = Swal.getPopup().querySelector('#verification-code');
-                const verificationCodeValue = verificationCode.value;
+            didOpen() {
+                let inputVerificationCode = Swal.getPopup().querySelector('#verification-code');
 
-                if (verificationCodeValue.length != 6) {
-                    Swal.showValidationMessage("Verification Code must be 6 chars!");
-                }
-                if ((verificationCodeValue).match(/\D/)) {
-                    Swal.showValidationMessage("Please enter number only!");
-                }
-                if (!verificationCodeValue) {
-                    Swal.showValidationMessage(`Please enter Verification Code!`);
-                }
-                return {
-                    verification_code: verificationCodeValue
-                };
+                Swal.getPopup().querySelector('#next').addEventListener("click", () => {
+                    if (inputVerificationCode.value.length != 6) {
+                        Swal.showValidationMessage("Verification Code must be 6 chars!");
+                    } else if ((inputVerificationCode.value).match(/\D/)) {
+                        Swal.showValidationMessage("Please enter number only!");
+                    } else if (!inputVerificationCode.value) {
+                        Swal.showValidationMessage(`Please enter Verification Code!`);
+                    }
+
+                    if (typeof onConfirmCallback == "function") {
+                        onConfirmCallback(inputVerificationCode.value);
+                    }
+                });
+
+                Swal.getPopup().querySelector('#resend').addEventListener("click", async event => {
+                    let response;
+
+                    if (typeof resendCallbackOrObjectOrEmail == "function") {
+                        resendCallbackOrObjectOrEmail();
+                    } else if (typeof resendCallbackOrObjectOrEmail == "object") {
+                        response = await AuthService.createVerificationCode(resendCallbackOrObjectOrEmail.email);
+                    } else if (typeof resendCallbackOrObjectOrEmail == "string") {
+                        response = await AuthService.createVerificationCode(resendCallbackOrObjectOrEmail)
+                    } else {
+                        null; // still on progress build  
+                    }
+
+                    if (response.status == 429 && typeof resendCallbackOrObjectOrEmail != "function") {
+                        new Promise(() => {
+                            let timer60Sec = 60;
+                            let resendInterval = setInterval(() => {
+                                timer60Sec -= 1;
+                                event.target.disabled = true;
+                                event.target.childNodes[1].innerHTML = `(${timer60Sec})`;
+
+                                if (!timer60Sec) {
+                                    event.target.disabled = false;
+                                    event.target.childNodes[1].innerHTML = "";
+                                    clearInterval(resendInterval);
+                                }
+                            }, 1000);
+                        })
+                    }
+                });
             }
         });
     }
@@ -65,7 +107,6 @@ export default class SwalPlugin {
                 Swal.showLoading();
             }
         })
-
         // if u want to use "callback" parameter/argument of this function please make sure your callback returning a value and also asynchronus function/callback; 
         if (typeof callback == "function") {
             const resultOfCallback = await callback().then(
@@ -75,5 +116,19 @@ export default class SwalPlugin {
         }
         // if "callback" parameter/argument of this function is null or not a function, then return Swal;  
         return Swal;
+    }
+
+    static async autoCloseAlert(title, html, timer = 2000) {
+        return await Swal.fire({
+            title,
+            html,
+            timer,
+            timerProgressBar: true,
+            allowOutsideClick: false,
+            allowEscapeKey: false,
+            didOpen: () => {
+                Swal.showLoading()
+            },
+        });
     }
 }
