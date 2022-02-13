357ffbcb8ec66814291225cd54d087352374aeb1
mysql73 <arfan2173@gmail.com>
2022-02-13 19:13:50 +0700

    refactoring

--- src/views/SendAndRequest.vue
+++ src/views/SendAndRequest.vue
@@ -3,30 +3,12 @@
         <NavbarTop />
         <div class="row p-0 m-0">
             <div class="col-md-8 bg-white">
-                <SendPaymentTo v-if="state.currentMenu == `send`" />
-                <RequestPaymentFrom v-if="state.currentMenu == `request`" />
-                <Contacts v-if="state.currentMenu == `contacts`" />
+                <SendPaymentTo v-if="state.currentMenu == state.rightSideMenu[0].name" />
+                <RequestPaymentFrom v-if="state.currentMenu == state.rightSideMenu[1].name" />
+                <Contacts v-if="state.currentMenu == state.rightSideMenu[2].name" />
             </div>
             <div class="col-md-4">
-                <div class="bg-white ms-4">
-                    <ul class="text-end p-0">
-                        <router-link
-                            to="/transfer/send"
-                            @click="changeMenu(`send`)"
-                            class="py-3 px-3 border-bottom cursor-pointer d-block text-decoration-none text-dark fw-bolder"
-                        >Send</router-link>
-                        <router-link
-                            to="/transfer/request"
-                            @click="changeMenu(`request`)"
-                            class="py-3 px-3 border-bottom cursor-pointer d-block text-decoration-none text-dark fw-bolder"
-                        >Request</router-link>
-                        <router-link
-                            to="/transfer/contacts"
-                            @click="changeMenu(`contacts`)"
-                            class="py-3 px-3 border-bottom cursor-pointer d-block text-decoration-none text-dark fw-bolder"
-                        >Contacts</router-link>
-                    </ul>
-                </div>
+                <RightSideMenu :menus="state.rightSideMenu" @menuClicked="changeMenu" />
             </div>
         </div>
     </div>
@@ -36,22 +18,30 @@
 import NavbarTop from '../components/NavbarTop.vue';
 import SendPaymentTo from '../components/SendAndRequest/SendPaymentTo.vue';
 import RequestPaymentFrom from '../components/SendAndRequest/RequestPaymentFrom.vue';
+import RightSideMenu from '../components/RightSideMenu.vue';
 import Contacts from "@/components/SendAndRequest/Contacts.vue";
-import router from '../router';
+
 import { defineComponent, onMounted, onBeforeMount, reactive, onUpdated } from 'vue';
-import { useRoute } from 'vue-router';
-const route = useRoute();
+// import { useRoute } from 'vue-router';
+// const route = useRoute();
 const state = reactive({
-    currentMenu: "send"
+    currentMenu: "Send Money",
+    rightSideMenu: [{
+        name: "Send Money", link: "/transfer/send"
+    }, {
+        name: "Request Money", link: "/transfer/request"
+    }, {
+        name: "Contacts", link: "/transfer/contacts"
+    }],
 });
 defineComponent({
-    NavbarTop, SendPaymentTo, RequestPaymentFrom, Contacts
+    NavbarTop, SendPaymentTo, RequestPaymentFrom, Contacts, RightSideMenu
 });
 onBeforeMount(() => {
-    changeMenu(route.params.menu);
+    changeMenu(state.rightSideMenu[0].name);
 });
 onMounted(() => {
-    // keep the nav link active after click menus (components )
+    // keep the nav link active after click menus (components)
     document.getElementById("NavLinkSendandRequest").classList.add("router-link-active")
     document.body.style.height = "2000px";
 });
@@ -62,35 +52,18 @@ onUpdated(() => {
 
 
 function changeMenu(menu) {
-    state.currentMenu = menu;
-    const menus = ["send", "request", "contacts"];
+    state.currentMenu = menu.name;
+    const menuNames = state.rightSideMenu.map(elem => elem.name);
+
     switch (menu) {
-        case menus[1]:
-            state.currentMenu = menus[1];
-            router.replace({
-                ...router.currentRoute,
-                params: {
-                    menu: menus[1]
-                }
-            })
+        case menuNames[1]:
+            state.currentMenu = menuNames[1];
             break;
-        case menus[2]:
-            state.currentMenu = menus[2];
-            router.replace({
-                ...router.currentRoute,
-                params: {
-                    menu: menus[2]
-                }
-            })
+        case menuNames[2]:
+            state.currentMenu = menuNames[2];
             break;
-        default:
-            state.currentMenu = menus[0];
-            router.replace({
-                ...router.currentRoute,
-                params: {
-                    menu: menus[0]
-                }
-            })
+        case menuNames[0]:
+            state.currentMenu = menuNames[0];
     }
 }
 </script>
