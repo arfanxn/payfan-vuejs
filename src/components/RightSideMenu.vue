7a90dacba0296bd2c675871543d202fc6f98bb63
mysql73 <arfan2173@gmail.com>
2022-02-13 19:13:20 +0700

    make components for menu on right side

---
+++ src/components/RightSideMenu.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="bg-white ms-4">
+        <ul class="text-end p-0">
+            <router-link
+                v-for="(menu, index) in props.menus"
+                :key="index"
+                :to="menu.link || menu.url"
+                @click.prevent="menuClicked(menu)"
+                class="py-3 px-3 border-bottom cursor-pointer d-block text-decoration-none text-dark fw-bolder"
+            >{{ menu.name || menu.text || menu.title }}</router-link>
+        </ul>
+    </div>
+</template>
+
+<script setup>
+import { defineProps, defineEmits } from "vue";
+const emits = defineEmits(["menuClicked"]);
+const props = defineProps({ menus: Array });
+
+
+function menuClicked(menu) {
+    emits("menuClicked", menu);
+}
+</script> 
+
+<style scoped>
+.router-link-active {
+    color: rgb(0, 112, 186) !important;
+    border-color: rgb(0, 112, 186) !important;
+    border-bottom-width: 3px !important;
+}
+</style>
