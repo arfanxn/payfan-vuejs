3b498a3b5a73282692f70b3298186c7a5357d2cd
mysql73 <arfan2173@gmail.com>
2022-02-13 15:19:29 +0700

    refactoring

--- src/store/modules/activity.js
+++ src/store/modules/activity.js
@@ -1,3 +1,5 @@
+import Helpers from "../../Helpers";
+
 export default {
     namespaced: true,
     // getters: {
@@ -24,10 +26,18 @@ export default {
     }),
     mutations: {
         updateFilterBy(state, value) {
-            if (typeof value.transaction_type != "undefined")
-                state.filter_by.transaction_type = value.transaction_type;
-            if (typeof value.status != "undefined")
-                state.filter_by.status = value.status;
+            if (typeof value.transaction_type != "undefined") {
+                if (value.transaction_type == null)
+                    state.filter_by.transaction_type = null;
+                else if (Helpers.arrayContains(state.filter_options.transaction_type, value.transaction_type))
+                    state.filter_by.transaction_type = value.transaction_type;
+            }
+            if (typeof value.status != "undefined") {
+                if (value.status == null)
+                    state.filter_by.status = null;
+                else if (Helpers.arrayContains(state.filter_options.status, value.status))
+                    state.filter_by.status = value.status;
+            }
         }
     },
     actions: {}
