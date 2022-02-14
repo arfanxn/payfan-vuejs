b2c26cede651f22aba54fa4ca5c1217d1006bd5d
mysql73 <arfan2173@gmail.com>
2022-02-15 00:38:04 +0700

    make modals

---
+++ src/components/Bootstrap5/Modals/BlockedContactsModal.vue
@@ -0,0 +1,78 @@
+<template>
+    <teleport to="#modals">
+        <!-- Button trigger modal -->
+        <button
+            id="btn-modal-blocked-contacts"
+            type="button"
+            class="btn d-none btn-primary"
+            data-bs-toggle="modal"
+            data-bs-target="#modal-blocked-contacts"
+        >Launch demo modal</button>
+
+        <!-- Modal -->
+        <div
+            class="modal fade"
+            id="modal-blocked-contacts"
+            tabindex="-1"
+            aria-labelledby="modal-contant-detail"
+            aria-hidden="true"
+        >
+            <div class="modal-dialog modal-lg">
+                <div class="modal-content">
+                    <div class="modal-header border-0">
+                        <button
+                            type="button"
+                            class="btn-close"
+                            data-bs-dismiss="modal"
+                            aria-label="Close"
+                        ></button>
+                    </div>
+                    <div class="modal-body px-5 pb-5 d-flex flex-column">
+                        <h5 class="text-center mb-4">Blocked Contacts</h5>
+                        <!-- <span class="text-secondary mb-5 pb-5">No blocked contacts yet.</span> -->
+                        <div
+                            class="d-flex justify-content-between cursor-pointer border-bottom border-secondary py-3 px-3"
+                            v-for="(item, index) in [1, 2, 3]"
+                            :key="index"
+                        >
+                            <a class="d-flex text-dark w-100 me-2">
+                                <div
+                                    class="contact-icon-size shadow bg-warning rounded-circle d-flex justify-content-center"
+                                >
+                                    <span class="fw-bold my-auto">AM</span>
+                                </div>
+                                <span class="ms-3 my-auto">Alif Maulana</span>
+                            </a>
+
+                            <a
+                                @click="unblockContact(index)"
+                                :id="`top-contact-${index}`"
+                                class="my-auto text-navy fw-bold hover-underline"
+                            >Unblock</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </teleport>
+</template>
+
+<script setup>
+import { defineComponent } from "@vue/runtime-core";
+import StarIcon from "@/components/Icons/StarIcon.vue";
+
+defineComponent({ StarIcon })
+
+</script>
+
+<style scoped>
+.contact-icon-size {
+    width: 75px;
+    height: 75px;
+}
+
+.contact-icon-size {
+    width: 60px;
+    height: 60px;
+}
+</style>
---
+++ src/components/Bootstrap5/Modals/NotificationSettingsModal.vue
@@ -0,0 +1,72 @@
+<template>
+    <teleport to="#modals">
+        <!-- Button trigger modal -->
+        <button
+            id="btn-modal-notification-settings"
+            type="button"
+            class="btn d-none btn-primary"
+            data-bs-toggle="modal"
+            data-bs-target="#modal-notification-settings"
+        >Launch demo modal</button>
+
+        <!-- Modal -->
+        <div
+            class="modal fade"
+            id="modal-notification-settings"
+            tabindex="-1"
+            aria-labelledby="modal-contant-detail"
+            aria-hidden="true"
+        >
+            <div class="modal-dialog modal-lg">
+                <div class="modal-content">
+                    <div class="modal-header border-0">
+                        <button
+                            type="button"
+                            class="btn-close"
+                            data-bs-dismiss="modal"
+                            aria-label="Close"
+                        ></button>
+                    </div>
+                    <div class="modal-body px-5 pb-5 d-flex flex-column">
+                        <h5 class="text-center mb-4">Notifications Settings</h5>
+
+                        <div>
+                            <span class="text-secondary">We'll let you know when you</span>
+                            <div
+                                class="cursor-pointer py-4 border-bottom border-secondary d-flex justify-content-between"
+                            >
+                                <span>Make a payment</span>
+
+                                <a class="cursor-pointer hover-underline text-navy fw-bold">On</a>
+                            </div>
+                            <div
+                                class="cursor-pointer py-4 border-bottom border-secondary d-flex justify-content-between"
+                            >
+                                <span>Request a payment</span>
+                                <a
+                                    class="cursor-pointer hover-underline text-navy fw-bold ps-2 d-block"
+                                >Off</a>
+                            </div>
+                            <div
+                                class="cursor-pointer py-4 border-bottom border-secondary d-flex justify-content-between"
+                            >
+                                <span>Receive a payment</span>
+
+                                <a class="cursor-pointer hover-underline text-navy fw-bold">Off</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </teleport>
+</template>
+
+<script setup>
+import { defineComponent } from "@vue/runtime-core";
+import StarIcon from "@/components/Icons/StarIcon.vue";
+
+defineComponent({ StarIcon })
+
+</script>
+
