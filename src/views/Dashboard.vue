71757ace623157fe08bfa5054e150ca81b95a348
mysql73 <arfan2173@gmail.com>
2022-02-24 07:04:38 +0700

    integrate dashborad with userStore

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -3,7 +3,7 @@
     <NavbarTop />
     <div class="row mt-2 p-0 m-0">
       <div class="col-md-7 p-0 m-0">
-        <h2 class="mb-4">Hello, Muhammad Arfan.</h2>
+        <h2 class="mb-4">Hello, {{ UserStore.self.name }}</h2>
         <WalletCard />
       </div>
       <div class="col-md-5 p-0 ps-5 pe-4 m-0">
@@ -24,11 +24,17 @@ import WalletCard from '../components/Dashboard/WalletCard.vue';
 import TransferMenu from '../components/Dashboard/TransferMenu.vue';
 import RecentActivity from '../components/Dashboard/RecentActivity.vue';
 import SendAgain from '../components/Dashboard/SendAgain.vue';
-import { defineComponent } from 'vue';
+import { defineComponent, onMounted } from 'vue';
+import { useUserStore } from '@/stores/UserStore.js';
+const UserStore = useUserStore();
 
 defineComponent({
   NavbarTop, TransferMenu, WalletCard, RecentActivity, SendAgain
 })
+
+onMounted(() => {
+  UserStore.fetchSelf()
+});
 
 </script>
 
