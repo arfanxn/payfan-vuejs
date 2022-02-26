af9e5a1746bafab8ddf6bf3d164836fdff487148
mysql73 <arfan2173@gmail.com>
2022-02-26 23:54:42 +0700

    add search feature

--- src/components/SendAndRequest/RequestPaymentFrom.vue
+++ src/components/SendAndRequest/RequestPaymentFrom.vue
@@ -1,20 +1,15 @@
 <template>
     <div class="ms-4 p-5">
         <h5 class="text-dark mb-4">Request Payment From</h5>
-        <div class="form-floating mb-4">
-            <input
-                type="email"
-                class="form-control"
-                id="inputSendTo"
-                placeholder="Name on your contacts or Email"
-            />
-            <label class="text-secondary" for="inputSendTo">Name on your contacts or Email</label>
-        </div>
-        <button class="btn btn-light border-dark rounded-pill py-2 px-4">Next</button>
+        <FieldForSearchPeople />
     </div>
 </template>
 
-<script setup></script>
+<script setup>
+import FieldForSearchPeople from './FieldForSearchPeople.vue';
+import { defineComponent } from "@vue/runtime-core";
+defineComponent({ FieldForSearchPeople });
+</script>
 
 <style scoped>
 </style>
--- src/components/SendAndRequest/SendPaymentTo.vue
+++ src/components/SendAndRequest/SendPaymentTo.vue
@@ -1,24 +1,16 @@
 <template>
     <div class="ms-4 p-5">
         <h5 class="text-dark mb-4">Send Payment To</h5>
-        <div class="form-floating mb-4">
-            <input
-                type="email"
-                class="form-control"
-                id="inputSendTo"
-                placeholder="Name on your contacts or Email"
-            />
-            <label class="text-secondary" for="inputSendTo">Name on your contacts or Email</label>
-            <!-- <DropdownInputContactOption /> -->
-        </div>
-        <button class="btn btn-light border-dark rounded-pill py-2 px-4">Next</button>
+        <FieldForSearchPeople />
     </div>
 </template>
 
 <script setup>
-import DropdownInputContactOption from "@/components/SendAndRequest/DropdownInputContactOption.vue";
+import FieldForSearchPeople from './FieldForSearchPeople.vue';
 import { defineComponent } from "@vue/runtime-core";
-defineComponent({ DropdownInputContactOption }); 
+defineComponent({ FieldForSearchPeople });
+
+
 </script>
 
 <style scoped>
