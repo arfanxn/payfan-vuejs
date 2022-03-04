02f628de19f377458ed0c678bdd9811273bcca3c
mysql73 <arfan2173@gmail.com>
2022-03-05 06:41:45 +0700

    rename file && change import file name

--- src/components/SendAndRequest/RequestPaymentFrom.vue
+++ src/components/SendAndRequest/RequestPaymentFrom.vue
@@ -1,14 +1,14 @@
 <template>
     <div class="ms-4 p-5">
         <h5 class="text-dark mb-4">Request Payment From</h5>
-        <FieldForSearchPeople />
+        <FieldSearchPeople />
     </div>
 </template>
 
 <script setup>
-import FieldForSearchPeople from './FieldForSearchPeople.vue';
+import FieldSearchPeople from './FieldSearchPeople.vue';
 import { defineComponent } from "@vue/runtime-core";
-defineComponent({ FieldForSearchPeople });
+defineComponent({ FieldSearchPeople });
 </script>
 
 <style scoped>
--- src/components/SendAndRequest/SendPaymentTo.vue
+++ src/components/SendAndRequest/SendPaymentTo.vue
@@ -1,14 +1,14 @@
 <template>
     <div class="ms-4 p-5">
         <h5 class="text-dark mb-4">Send Payment To</h5>
-        <FieldForSearchPeople />
+        <FieldSearchPeople @peopleClicked="test" />
     </div>
 </template>
 
 <script setup>
-import FieldForSearchPeople from './FieldForSearchPeople.vue';
+import FieldSearchPeople from './FieldSearchPeople.vue';
 import { defineComponent } from "@vue/runtime-core";
-defineComponent({ FieldForSearchPeople });
+defineComponent({ FieldSearchPeople });
 
 
 </script>
