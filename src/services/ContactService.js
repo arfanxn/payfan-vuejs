347d54f642db43501a4ac956551f5d83fe21713f
mysql73 <arfan2173@gmail.com>
2022-03-05 02:38:43 +0700

    add some contacts crud

--- src/services/ContactService.js
+++ src/services/ContactService.js
@@ -1,10 +1,50 @@
 import axios from "axios";
+import {
+    useContactStore
+} from "../stores/ContactStore";
+const ContactStore = useContactStore();
 
 export default class ContactService {
-    static async whereNotBlocked() {
+    static async topContacts() {
         try {
             const response = await axios.get("/api/user/self/contacts/top-contacts");
 
+            return response;
+        } catch (error) {
+            return error.response;
+        }
+    }
+
+    static async lastTransactionDetail(contactID) {
+        try {
+            return await axios.get(`/api/user/self/contact/${contactID}/last-transaction`);
+        } catch (error) {
+            return error.response;
+        }
+    }
+
+    static async addOrRm(userID) {
+        try {
+            return await axios.post(`/api/user/self/contact/add-or-rm/user/${userID}`);
+        } catch (error) {
+            return error.response;
+        }
+    }
+
+    static async block(contactID) {
+        try {
+            return await axios.post(`/api/user/self/contact/${contactID}/block`);
+        } catch (error) {
+            return error.response;
+        }
+    }
+
+    static async toggleFavorite(contactID) {
+        try {
+            const response = await axios.get(`/api/user/self/contact/${contactID}/toggle-favorite`);
+            if (response) {
+                ContactStore.toggleFavorite(contactID)
+            }
             return response;
         } catch (error) {
             return error.response;
