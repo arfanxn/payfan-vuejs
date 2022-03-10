6e7a487af9767ad973786d199fe7f2bddf0cac1d
mysql73 <arfan2173@gmail.com>
2022-03-10 08:18:30 +0700

    adding  handleSendPayment function

--- src/services/functions.js
+++ src/services/functions.js
@@ -1,4 +1,8 @@
 import axios from "axios";
+import SwalPlugin from '@/plugins/SwalPlugin.js'
+import TransactionService from '@/services/TransactionService.js'
+import Helpers from '@/Helpers.js'
+import AuthService from '@/services/AuthService.js'
 
 export const searchPeoplesOnPayfan = async (keyword) => {
     try {
@@ -13,3 +17,37 @@ export const searchPeoplesOnPayfan = async (keyword) => {
         return error.response;
     }
 }
+
+export const handleSendPayment = ({
+    amount,
+    name,
+    note,
+    wallet,
+    amountInUSD
+}) => {
+    SwalPlugin.confirm({
+        title: 'Confirm your payment',
+        html: `Send money ${amountInUSD} to "${name}" ?`,
+        icon: 'question'
+    }).then(result => {
+        if (result.isConfirmed) {
+            Helpers.closeBSModal(`#btn-close-modal-transfer-preview`).then(() => {
+                AuthService.createVerificationCode().then(() => {
+                    SwalPlugin.verificationCode("Verify to continue", async verificationCode => {
+                        return await TransactionService.sendMoney({
+                            amount,
+                            note,
+                            to_wallet: wallet['address'],
+                            code: verificationCode,
+                        }).then(r => {
+                            if (r.status == 200) {
+                                SwalPlugin.autoCloseAlert('Send Money success', `Send money to "${name}", amount ${amountInUSD} successfully.`, "success", 2000);
+                            }
+                            return r;
+                        })
+                    })
+                })
+            });
+        }
+    });
+};
