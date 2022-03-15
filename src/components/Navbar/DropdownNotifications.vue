ffcee6c5f523b82a23db42bd610d05ba71a0a0f6
mysql73 <arfan2173@gmail.com>
2022-03-15 22:38:25 +0700

    commit

--- src/components/Navbar/DropdownNotifications.vue
+++ src/components/Navbar/DropdownNotifications.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="dropdown">
+    <div class="dropdown text-break">
         <a
             href="#"
             class="nav-link fw-bold fs-6 d-flex"
@@ -13,21 +13,122 @@
             />
         </a>
         <ul
-            class="dropdown-menu pt-0 notification-dropdown-menu"
+            class="dropdown-menu pt-0 notification-dropdown-menu overflow-auto"
+            @click="e => e.stopPropagation() /* preventing close dropdown when clicked  */"
+            @scroll="loadMoreNotifications"
             aria-labelledby="dropdownMenuButton"
         >
-            <header class="py-1 d-flex justify-content-center">
-                <span class="my-auto text-white fw-bold">Notifications (0)</span>
-            </header>
-            <li v-for="(i, index ) in [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]" :key="index">
-                <a class="dropdown-item" href="#">Notification {{ i }}</a>
+            <li class="dropdown-header-wrapper d-flex py-1 justify-content-center">
+                <h5 class="dropdown-header text-white fs-6 fw-bold my-auto">
+                    Notifications
+                    <span>({{ NotificationStore['latest/total_unread'] }})</span>
+                </h5>
             </li>
+            <li
+                class="text-wrap cursor-pointer my-1"
+                v-for="(notification, index ) in NotificationStore[`latest/data`]"
+                :key="index"
+            >
+                <a class="dropdown-item text-start text-wrap py-2">
+                    <div :class="notification.read_at ? `text-secondary` : `text-dark`">
+                        <h6 class="fw-bold my-0 py-0">
+                            {{
+                                StrHelper.make(notification.type.substring(notification.type.lastIndexOf("\\") + 1)).removeBackslashes().addSpaceBeforeCapitals().get().replace(/(notificatio[n|ns]+)/ig, "")
+                            }}
+                        </h6>
+                        <span class="text-break" href="#">{{ notification.data.text }}</span>
+                    </div>
+
+                    <button
+                        v-if="notification.data?.link && notification.data?.action"
+                        class="d-flex justify-content-start me-1 btn btn-outline-primary mt-1 py-0 px-2 rounded-pill"
+                    >
+                        <small class="my-auto">{{ notification.data.action }}</small>
+                    </button>
+                    <button
+                        @click="toogleNotificationRead(notification)"
+                        class="d-flex justify-content-start btn btn-outline-primary mt-1 py-0 px-2 rounded-pill"
+                    >
+                        <small
+                            class="my-auto"
+                        >{{ notification.read_at ? "Mark as unread" : "Mark as read" }}</small>
+                    </button>
+                </a>
+            </li>
+
+            <!-- blank li for adding a range between  -->
+            <li
+                class="pb-5 pt-3 mb-5 bg-transparent text-center fw-bold"
+            >{{ notification.isLoadingMore ? `Loading more notifications....` : `No more notificitaions yet.` }}</li>
         </ul>
     </div>
 </template>
 
+<script setup>
+import { onMounted, reactive, } from 'vue';
+import { useNotificationStore } from '../../stores/NotificationStore';
+import NotificationService from "@/services/NotificationService.js";
+import StrHelper from "@/helpers/StrHelper.js"
+const NotificationStore = useNotificationStore()
+const notification = reactive({
+    alreadyLoadedPageList: [],
+    isLoadingMore: false,
+});
+
+onMounted(() => {
+    NotificationService.latest().then(r => {
+        if (r.status == 200) {
+            notification.alreadyLoadedPageList.push(r.data.notifications['current_page']);
+            NotificationStore.refillLatest(r.data.notifications);
+
+        }
+    })
+})
+
+function loadMoreNotifications(event) {
+    if (event.target.offsetHeight + event.target.scrollTop >= event.target.scrollHeight) { // if scrolled to bottom 
+        notification.isLoadingMore = true;
+        const nextPage = NotificationStore['latest/meta']["current_page"] + 1;
+        if (notification.alreadyLoadedPageList.indexOf(nextPage) <= -1) { // if next page isn't loaded 
+            NotificationService.latest(nextPage).then(r => {
+                if (r.status == 200) {
+                    const lastIndexOfNotificationsData = r.data['notifications']['data'].length - 1;
+                    const isAlreadyInsertedInNotificationStore = NotificationStore['latest/data']
+                        .filter(notification => notification.id ==
+                            r.data['notifications']['data'][lastIndexOfNotificationsData]["id"])
+                        .length;
+
+                    if (!isAlreadyInsertedInNotificationStore) {
+                        NotificationStore.refillLatest(r.data.notifications);
+                        notification.alreadyLoadedPageList.push(r.data.notifications['current_page']);
+                        notification.isLoadingMore = false;
+                    }
+                }
+            });
+        }
+    }
+}
+
+const toogleNotificationRead = (notification) => {
+    if (notification.read_at) {
+        NotificationService.markAsUnread(notification.id).then(r => {
+            if (r.status == 200) {
+                NotificationStore.markAsUnread(notification.id);
+            }
+        });
+    } else {
+        NotificationService.markAsRead(notification.id).then(r => {
+            if (r.status == 200) {
+                NotificationStore.markAsRead(notification.id)
+            }
+        });
+    }
+}
+
+</script>
+
 <style scoped>
-.notification-dropdown-menu header {
+.dropdown-header-wrapper {
     background-color: #0070ba !important;
 }
 
@@ -37,6 +138,10 @@
     left: 50% !important;
     transform: translateX(-50%) !important;
     top: 100% !important;
+}
+
+.dropdown-item:active {
+    background: #fff !important;
 }
 
 .nav-link-icon {
