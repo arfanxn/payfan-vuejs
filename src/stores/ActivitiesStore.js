9d0e3de6d0371527d140b2951253837c19f4f3e6
mysql73 <arfan2173@gmail.com>
2022-03-25 06:41:14 +0700

    creating activititiesStore pagination

--- src/stores/ActivitiesStore.js
+++ src/stores/ActivitiesStore.js
@@ -2,27 +2,45 @@
 import {
     defineStore
 } from "pinia";
+import axios from "axios";
 import Helpers from "../Helpers";
 
 export const useActivitiesStore = defineStore("activities", {
     state: () => ({
+        "pagination/data": {},
+        "pagination/meta": {},
         filter: {
             by: {
+                keyword: "",
+                start_at: null,
+                end_at: null,
                 transaction_type: null,
                 status: null
             },
             option: {
                 status: ["Completed", "Pending", "Failed"],
-                transaction_type: ["Send", "Request", "Received"]
+                transaction_type: ["Send", "Request", "Reward"]
             }
         }
     }),
 
     actions: {
         updateFilterBy({
+            keyword,
+            start_at,
+            end_at,
             status,
             transaction_type
         }) {
+            if (typeof keyword != "undefined") {
+                this.filter.by.keyword = !keyword ? "" : keyword;
+            }
+            if (typeof start_at != "undefined") {
+                this.filter.by.start_at = start_at == null ? null : start_at;
+            }
+            if (typeof end_at != "undefined") {
+                this.filter.by.end_at = end_at == null ? null : end_at;
+            }
             if (typeof transaction_type != "undefined") {
                 if (transaction_type == null)
                     this.filter.by.transaction_type = null;
@@ -34,6 +52,38 @@ export const useActivitiesStore = defineStore("activities", {
                     this.filter.by.status = null;
                 else if (Helpers.arrayContains(this.filter.option.status, status))
                     this.filter.by.status = status;
+            }
+        },
+        async fetch() {
+            try {
+                const {
+                    keyword,
+                    start_at,
+                    end_at,
+                    status,
+                    transaction_type
+                } = this.filter.by;
+                const response = await axios.get("/api/user/self/orders", {
+                    // const response = await axios.get("/api/orders", {
+                    params: {
+                        keyword: keyword || "",
+                        start_at: start_at || null,
+                        end_at: end_at || null,
+                        type: transaction_type || null,
+                        status: status || null
+                    }
+                });
+
+                if (response.status == 200) {
+                    this["pagination/data"] = response.data.orders.data;
+
+                    if (delete response.data.orders.data)
+                        this["pagination/meta"] = response.data.orders;
+                }
+
+                return response;
+            } catch (error) {
+                return error.response;
             }
         }
     }
