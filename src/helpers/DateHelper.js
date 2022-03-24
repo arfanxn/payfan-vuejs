47723bfe334b19695e04c4155697fea16d5b119c
mysql73 <arfan2173@gmail.com>
2022-03-25 06:39:52 +0700

    add method and fixed bug

--- src/helpers/DateHelper.js
+++ src/helpers/DateHelper.js
@@ -9,7 +9,19 @@ class DateHelper {
             "June", "August", "September", "October", 'November', 'December'
         ];
 
-        return (monthNames[month - 1]).slice(0, length)
+        return (monthNames[month]).slice(0, length)
+    }
+
+    now(format = null) {
+        if (format == "carbon" || !format) {
+            const now = new Date();
+            let carbonFormat = `${now.getFullYear()}-${now.getMonth().length < 2 ?
+                "0" + now.getMonth() : now.getMonth()}-${now.getDate().length < 2 ?
+                "0" + now.getDate() : now.getDate()} ${now.getHours().length < 2 ?
+                "0" + now.getHours() : now.getHours()}:${now.getMinutes().length < 2 ?
+                "0" + now.getMinutes() : now.getMinutes()}:${now.getSeconds()}`;
+            return carbonFormat;
+        }
     }
 }
 
