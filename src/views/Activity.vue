a011a8ad7f107a26ef0226eaaaeca26aeb9e71c4
mysql73 <arfan2173@gmail.com>
2022-04-02 23:50:51 +0700

    fixed pagination bug

--- src/views/Activity.vue
+++ src/views/Activity.vue
@@ -26,33 +26,35 @@
                         }}
                     </small>
                     <ActivityList :key="index" :activities="activities" />
-                    <div class="mt-4 d-flex justify-content-between">
-                        <h6
-                            class="fw-bold"
-                        >Current Page : {{ ActivitiesStore['pagination/meta']['current_page'] }}</h6>
-                        <div>
-                            <button
-                                :disabled="ActivitiesStore['pagination/meta']['current_page'] <= 1 ? true : false"
-                                class="btn btn-outline-secondary me-3"
-                                @click="loadActivitiesPagination(1)"
-                            >First</button>
-                            <button
-                                :disabled="ActivitiesStore['pagination/meta']['current_page'] <= 1 ? true : false"
-                                class="btn btn-primary mx-1"
-                                @click="loadActivitiesPagination(
-                                    parseInt(ActivitiesStore['pagination/meta']['current_page']) - 1
-                                )"
-                            >Prev</button>
-                            <button
-                                class="btn btn-primary"
-                                :disabled="ActivitiesStore['pagination/meta']['total_retrived'] < ActivitiesStore['pagination/meta']['per_page']"
-                                @click="loadActivitiesPagination(
-                                    parseInt(ActivitiesStore['pagination/meta']['current_page']) + 1
-                                
-                                )"
-                            >Next</button>
-                        </div>
-                    </div>
+                </div>
+            </div>
+            <!--  -->
+
+            <div class="mt-4 d-flex justify-content-between">
+                <h6
+                    class="fw-bold"
+                >Current Page : {{ ActivitiesStore['pagination/meta']['current_page'] }}</h6>
+                <div>
+                    <button
+                        :disabled="ActivitiesStore['pagination/meta']['current_page'] <= 1 ? true : false"
+                        class="btn btn-outline-secondary me-3"
+                        @click="loadActivitiesPagination(1)"
+                    >First</button>
+                    <button
+                        :disabled="ActivitiesStore['pagination/meta']['current_page'] <= 1 ? true : false"
+                        class="btn btn-primary mx-1"
+                        @click="loadActivitiesPagination(
+                            parseInt(ActivitiesStore['pagination/meta']['current_page']) - 1
+                        )"
+                    >Prev</button>
+                    <button
+                        class="btn btn-primary"
+                        :disabled="ActivitiesStore['pagination/meta']['total_retrived'] < ActivitiesStore['pagination/meta']['per_page']"
+                        @click="loadActivitiesPagination(
+                            parseInt(ActivitiesStore['pagination/meta']['current_page']) + 1
+                        
+                        )"
+                    >Next</button>
                 </div>
             </div>
         </div>
