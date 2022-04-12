29885d6b66cbba1ecbbd0a7390668235a7d289dc
mysql73 <arfan2173@gmail.com>
2022-04-12 23:04:24 +0700

    fixed file resource dependency

--- src/components/Layouts/NavbarTop.vue
+++ src/components/Layouts/NavbarTop.vue
@@ -2,56 +2,29 @@
     <teleport to="#navbar-top">
         <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-navy">
             <div class="container-md navbar-custom-container">
-                <router-link
-                    class="navbar-brand fw-bold fs-3"
-                    active-class="none"
-                    exact-active-class="none"
-                    :to="state.navbars[0]['link']"
-                >{{ Helpers.ENV("APP_TITLE") }}</router-link>
-                <button
-                    class="navbar-toggler"
-                    type="button"
-                    data-bs-toggle="collapse"
-                    data-bs-target="#navbarNavAltMarkup"
-                    aria-controls="navbarNavAltMarkup"
-                    aria-expanded="false"
-                    aria-label="Toggle navigation"
-                >
+                <router-link class="navbar-brand fw-bold fs-3" active-class="none" exact-active-class="none"
+                    :to="state.navbars[0]['link']">{{ Helpers.ENV("APP_TITLE") }}</router-link>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false"
+                    aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
-                <div
-                    class="collapse navbar-collapse d-md-flex justify-content-between"
-                    id="navbarNavAltMarkup"
-                >
+                <div class="collapse navbar-collapse d-md-flex justify-content-between" id="navbarNavAltMarkup">
                     <div class="navbar-nav d-flex justify-content-end">
-                        <router-link
-                            v-for="(navb, index ) in state.navbars"
-                            :id="navb.text == `Send and Request` ? `NavLinkSendandRequest` : null"
-                            :key="index"
+                        <router-link v-for="(navb, index ) in state.navbars"
+                            :id="navb.text == `Send and Request` ? `NavLinkSendandRequest` : null" :key="index"
                             class="nav-link nav-link-left-side d-flex px-3 mx-1 fs-6 fw-bold rounded-pill active"
-                            :to="navb.link"
-                        >
+                            :to="navb.link">
                             <small class="my-auto">{{ navb.text }}</small>
                         </router-link>
                     </div>
                     <div class="navbar-nav">
-                            <NotificationsDropdown />
-                        <router-link
-                            active-class="none"
-                            exact-active-class="none"
-                            to="/account/settings"
-                            class="cursor-pointer nav-link fw-bold fs-6 d-flex px-0 mx-1 invert-100"
-                        >
-                            <img
-                                class="nav-link-icon my-auto"
-                                src="../assets/icons/settings.png"
-                                alt="Settings"
-                            />
+                        <NotificationsDropdown />
+                        <router-link active-class="none" exact-active-class="none" to="/account/settings"
+                            class="cursor-pointer nav-link fw-bold fs-6 d-flex px-0 mx-1 invert-100">
+                            <img class="nav-link-icon my-auto" src="@/assets/icons/settings.png" alt="Settings" />
                         </router-link>
-                        <a
-                            class="cursor-pointer nav-link fw-bold fs-6 px-0 ms-2 me-1"
-                            @click.prevent="handleLogout()"
-                        >
+                        <a class="cursor-pointer nav-link fw-bold fs-6 px-0 ms-2 me-1" @click.prevent="handleLogout()">
                             <small>LOG OUT</small>
                         </a>
                     </div>
@@ -66,9 +39,9 @@
 <script setup>
 import Swal from 'sweetalert2';
 import { reactive, defineComponent } from 'vue';
-import Helpers from '../Helpers';
-import AuthService from '../services/AuthService';
-import NotificationsDropdown from './Navbar/NotificationsDropdown.vue';
+import Helpers from '@/Helpers';
+import AuthService from '@/services/AuthService';
+import NotificationsDropdown from '@/components/Navbar/NotificationsDropdown.vue';
 defineComponent({
     NotificationsDropdown
 })
