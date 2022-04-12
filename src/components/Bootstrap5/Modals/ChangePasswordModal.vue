aa3ca0aeca25486f89fa9041e3079d32e1f6d250
mysql73 <arfan2173@gmail.com>
2022-04-12 22:53:31 +0700

    input autocomplte off

--- src/components/Bootstrap5/Modals/ChangePasswordModal.vue
+++ src/components/Bootstrap5/Modals/ChangePasswordModal.vue
@@ -1,97 +1,52 @@
 <template>
     <teleport to="#modals">
         <!-- Button trigger modal -->
-        <button
-            id="btn-modal-change-password"
-            type="button"
-            class="btn d-none btn-primary"
-            data-bs-toggle="modal"
-            data-bs-target="#modal-change-password"
-        >Launch demo modal</button>
+        <button id="btn-modal-change-password" type="button" class="btn d-none btn-primary" data-bs-toggle="modal"
+            data-bs-target="#modal-change-password">Launch demo modal</button>
 
         <!-- Modal -->
-        <div
-            class="modal fade"
-            id="modal-change-password"
-            tabindex="-1"
-            aria-labelledby="modal-change-password"
-            aria-hidden="true"
-        >
+        <div class="modal fade" id="modal-change-password" tabindex="-1" aria-labelledby="modal-change-password"
+            aria-hidden="true">
             <div class="modal-dialog">
                 <div class="modal-content">
                     <div class="modal-header border-0">
-                        <button
-                            id="btn-close-modal-change-password"
-                            type="button"
-                            class="btn-close"
-                            data-bs-dismiss="modal"
-                            aria-label="Close"
-                        ></button>
+                        <button id="btn-close-modal-change-password" type="button" class="btn-close"
+                            data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
                     <div class="modal-body px-5 pb-5 d-flex flex-column text-center">
                         <h2 class="fw-light">Change your password</h2>
 
-                        <form class="text-start mt-4">
+                        <form class="text-start mt-4" autocomplete="off">
                             <div class="mb-3">
-                                <label
-                                    for="inputCurrentPassword"
-                                    class="form-label"
-                                >Confirm your current password</label>
-                                <input
-                                    v-model.lazy="state.current_password"
-                                    type="password"
-                                    class="form-control"
-                                    placeholder="Current password"
-                                    id="inputCurrentPassword"
-                                />
-                                <AlertError
-                                    class="mt-1"
-                                    @close="v$.$reset()"
-                                    :error="v$?.current_password?.$errors[0]?.$message"
-                                ></AlertError>
+                                <label for="inputCurrentPassword" class="form-label">Confirm your current
+                                    password</label>
+                                <input autocomplete="off" v-model.lazy="state.current_password" type="password"
+                                    class="form-control" placeholder="Current password" id="inputCurrentPassword" />
+                                <AlertError class="mt-1" @close="v$.$reset()"
+                                    :error="v$?.current_password?.$errors[0]?.$message"></AlertError>
                             </div>
                             <div class="mt-4">
-                                <div
-                                    id="emailHelp"
-                                    class="form-text text-dark mb-2"
-                                >Enter your new password (Keep your account more secure. Don't use your name.)</div>
-                                <input
-                                    v-model.lazy="state.password"
-                                    type="password"
-                                    class="form-control"
-                                    id="newPassword"
-                                    placeholder="New password"
-                                />
-                                <AlertError
-                                    class="mt-1"
-                                    @close="v$.$reset()"
-                                    :error="v$?.password?.$errors[0]?.$message"
-                                ></AlertError>
+                                <div id="emailHelp" class="form-text text-dark mb-2">Enter your new password (Keep your
+                                    account more secure. Don't use your name.)</div>
+                                <input v-model.lazy="state.password" type="password" class="form-control"
+                                    id="newPassword" placeholder="New password" />
+                                <AlertError class="mt-1" @close="v$.$reset()"
+                                    :error="v$?.password?.$errors[0]?.$message"></AlertError>
 
-                                <input
-                                    v-model.lazy="state.password_confirmation"
-                                    type="password"
-                                    class="form-control mt-3"
-                                    id="newPasswordConfirmation"
-                                    placeholder="Confirm new password"
-                                />
-                                <AlertError
-                                    class="mt-1"
-                                    @close="v$.$reset()"
-                                    :error="v$?.password_confirmation?.$errors[0]?.$message"
-                                ></AlertError>
+                                <input v-model.lazy="state.password_confirmation" type="password"
+                                    class="form-control mt-3" id="newPasswordConfirmation"
+                                    placeholder="Confirm new password" />
+                                <AlertError class="mt-1" @close="v$.$reset()"
+                                    :error="v$?.password_confirmation?.$errors[0]?.$message"></AlertError>
                             </div>
-                            <button
-                                @click.prevent="changePassword"
-                                type="button"
-                                class="btn btn-primary fw-bold rounded-pill text-white w-100 mt-4"
-                            >Submit</button>
+                            <button @click.prevent="changePassword" type="button"
+                                class="btn btn-primary fw-bold rounded-pill text-white w-100 mt-4">Submit</button>
                         </form>
                     </div>
                 </div>
             </div>
         </div>
-    </teleport>
+        </teleport>
 </template>
 
 <script setup>
@@ -109,9 +64,9 @@ defineComponent({
 });
 
 const state = reactive({
-    current_password: null,
-    password: null,
-    password_confirmation: null,
+    current_password: "",
+    password: "",
+    password_confirmation: "",
 });
 
 const v$ = useVuelidate({ // rules
