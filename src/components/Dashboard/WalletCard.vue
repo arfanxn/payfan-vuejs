b500ef0c0314b4b2344f14ff82ce12e6579e86ac
mysql73 <arfan2173@gmail.com>
2022-04-16 16:47:36 +0700

    live wallet update with pusher

--- src/components/Dashboard/WalletCard.vue
+++ src/components/Dashboard/WalletCard.vue
@@ -13,22 +13,54 @@
     <div class="dropstart cursor-pointer">
       <img class="vertical-dots dropdown-toggle" src="@/assets/icons/three-vertical-dots-black.png"
         data-bs-toggle="dropdown" aria-expanded="false" />
-      <ul class="dropdown-menu shadow">
-        <router-link class="dropdown-item text-dark" to="/help">Get Help</router-link>
+      <ul @click="e => e.stopPropagation()" class="dropdown-menu shadow pt-0">
+        <header class="bg-navy px-3 py-1 text-center">
+          <span class="text-white fw-bold">Wallet details</span>
+        </header>
+        <!-- <router-link class="dropdown-item text-dark" to="/help">Get Help</router-link> -->
+        <li class="dropdown-item text-dark">
+          Address : {{ AuthUserStore.wallet.data.address }}
+        </li>
+        <li class="dropdown-item text-dark">
+          Total Transaction : {{ AuthUserStore.wallet.data.total_transaction }}
+        </li>
+        <li v-show="AuthUserStore.wallet.data.last_transaction" class="dropdown-item text-dark">
+          Last Transaction : {{
+            Helpers.tap(new Date(AuthUserStore.wallet.data.last_transaction), last_transaction =>
+              `${last_transaction.getDate()} ${DateHelper.numericMonthtoString(last_transaction.getMonth(), 3)
+              } ${last_transaction.getFullYear()}`)
+          }}
+        </li>
       </ul>
-    </div>  </div>
+    </div>
+  </div>
 </template>
 
 <script setup>
+import { onMounted } from 'vue';
 import Helpers from '../../Helpers';
 import StrHelper from '../../helpers/StrHelper';
-import { useAuthUserStore } from '@/stores/auth/AuthUserStore.js';
+import DateHelper from '../../helpers/DateHelper';
+import { useAuthUserStore, } from '../../stores/auth/AuthUserStore.js';
 const AuthUserStore = useAuthUserStore();
+
+onMounted(() => {
+  // listen wallet changes
+  window.Echo.private('wallets.' + AuthUserStore.wallet.data['id'])
+    .listen("WalletUpdatedEvent", event => {
+      AuthUserStore.wallet.fill(event.wallet);
+    } /**/);
+});
+
 </script>
 
 <style scoped>
 .vertical-dots {
   width: 20px;
   height: 20px;
+}
+
+.dropdown-item:active {
+  background-color: #e9ecef !important;
 }
 </style>
